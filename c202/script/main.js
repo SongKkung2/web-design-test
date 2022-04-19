f8af57fdb0188889cb3c07b925f8d43f719587bd
SongKkung2 <grave_no@naver.com>
2022-04-19 13:00:00 +0900

    c2 문제 풀이 및, a1 문제 자료추가

--- c202/script/main.js
+++ c202/script/main.js
@@ -1,5 +1,38 @@
 $(function(){
     //1.popup
+    popup();
     //2.menu
+    menu();
     //3.slide
+    let intervalID = setInterval(slide, 3000)
 });
+//팝업 
+function popup(){
+    $(".news ul li:eq(0)").click(function(){
+        $("#popup").fadeIn();
+    })
+    $("#popup button").click(function(){
+        $("#popup").fadeOut();
+    })
+}
+//menu
+function menu(){
+    $("#gnb > li").mouseenter(function(){
+        $(this).children("a").addClass("highlight");
+        $(this).children(".lnb").stop().slideDown("fast");
+    })
+    $("#gnb > li").mouseleave(function(){
+        $(this).children(".lnb").stop().slideUp("fast");
+        $(this).children("a").removeClass("highlight");
+    })
+}
+
+function slide(){
+    let currentSlide = $(".slide > div.top");
+    let nextSlide = currentSlide.next();
+    if(!nextSlide.length){
+        nextSlide = $(".slide > div:first")
+    }
+    currentSlide.removeClass("top");
+    nextSlide.addClass("top");
+}
